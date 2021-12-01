716e6043ab997467bdcfc33f70cda77e75efeebe
Ryerzs <ryerzs@gmail.com>
2021-12-01 13:14:33 +0100

    Setup functions to be implemented

--- Day-1/Star-1.py
+++ Day-1/Star-1.py
@@ -2,5 +2,13 @@
 def main():
     pass
 
+# TODO: Implement a way to load data from txt file
+def load_data(path):
+    pass
+
+# TODO: Iterate through data vector and count increases in next iteration
+def count_increasing_iterations(data):
+    pass
+
 if __name__ == "__main__":
     main()
