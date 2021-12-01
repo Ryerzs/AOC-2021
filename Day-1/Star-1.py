a4c17277479f69e2e49342d22d6b7799120ae49f
Ryerzs <ryerzs@gmail.com>
2021-12-01 13:18:45 +0100

    Main operation

--- Day-1/Star-1.py
+++ Day-1/Star-1.py
@@ -1,6 +1,9 @@
 
 def main():
-    pass
+    path = "measurements.txt"
+    data = load_data(path)
+    count = count_increasing_iterations(data)
+    print(count)
 
 # TODO: Implement a way to load data from txt file
 def load_data(path: str) -> list:
