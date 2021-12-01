9ede2a57823f7a8b8f0b5c1de118b541903941b4
Ryerzs <ryerzs@gmail.com>
2021-12-01 13:16:58 +0100

    Add hinting for functions

--- Day-1/Star-1.py
+++ Day-1/Star-1.py
@@ -3,11 +3,11 @@ def main():
     pass
 
 # TODO: Implement a way to load data from txt file
-def load_data(path):
+def load_data(path: str) -> list:
     pass
 
 # TODO: Iterate through data vector and count increases in next iteration
-def count_increasing_iterations(data):
+def count_increasing_iterations(data: list) -> int:
     pass
 
 if __name__ == "__main__":
