a38fe774b24a62959107c491ca7a250574e49791
Ryerzs <ryerzs@gmail.com>
2021-12-06 08:13:45 +0100

    Added more default behavior in template

--- Day-/Star-2.py
+++ Day-/Star-2.py
@@ -1,6 +1,13 @@
+import time
+import numpy as np
 
 def main():
-    pass
+    path = "test-data.txt"
+    data_in = [int(el) for el in open(path, 'r').read().splitlines()[0].split(",")]
+    start_time = time.time()
+
+    dt = time.time() - start_time
+    print(dt)
 
 if __name__ == '__main__':
     main()
