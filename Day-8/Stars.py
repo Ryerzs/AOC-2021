f549e4f09a24c9074c23292e0e4965d4222e01fd
Ryerzs <ryerzs@gmail.com>
2021-12-08 16:14:00 +0100

    Written more functionally, a little bit more readable

--- Day-8/Stars.py
+++ Day-8/Stars.py
@@ -29,15 +29,15 @@ def star2(data_test, data_out):
         d_t = [set(el) for el in data_test[i]]
         d_o = [set(el) for el in data_out[i]]
         num = [""]*10
-        num[1] = find_one(d_t)
-        num[4] = find_four(d_t)
-        num[7] = find_seven(d_t)
-        num[8] = find_eight(d_t)
-        num[6] = find_six(d_t, num[7])
-        num[9] = find_nine(d_t, num[4])
-        num[3] = find_three(d_t, num[7])
-        num[5] = find_five(d_t, num[6])
-        num[0] = find_zero(d_t)
+        num[1] = find_general(d_t, cond_one, None)
+        num[4] = find_general(d_t, cond_four, None)
+        num[7] = find_general(d_t, cond_seven, None)
+        num[8] = find_general(d_t, cond_eight, None)
+        num[6] = find_general(d_t, cond_six, num[7])
+        num[9] = find_general(d_t, cond_nine, num[4])
+        num[3] = find_general(d_t, cond_three, num[7])
+        num[5] = find_general(d_t, cond_five, num[6])
+        num[0] = find_general(d_t, cond_zero, None)
         num[2] = d_t[0]
         total += get_output(d_o, num)
     return total
@@ -49,58 +49,37 @@ def get_output(data, num):
         number += str(num.index(el))
     return int(number)
 
-def find_zero(data):
-    for el in data:
-        if len(el) == 6:
-            data.remove(el) 
-            return el
+def cond_zero(a, _):
+    return len(a) == 6
 
-def find_one(data):
-    for el in data:
-        if len(el) == 2:
-            data.remove(el) 
-            return el
+def cond_one(a, _):
+    return len(a) == 2
 
-def find_three(data, seven):
-    for el in data:
-        if len(el & seven) == 3 and len(el) == 5:
-            data.remove(el) 
-            return el
+def cond_three(a, seven):
+    return len(a & seven) == 3 and len(a) == 5
 
-def find_four(data):
-    for el in data:
-        if len(el) == 4:
-            data.remove(el) 
-            return el
+def cond_four(a, _):
+    return len(a) == 4
 
-def find_five(data, six):
-    for el in data:
-        if len(el & six) == 5 and len(el) == 5:
-            data.remove(el) 
-            return el
+def cond_five(a, six):
+    return len(a & six) == 5 and len(a) == 5
 
-def find_six(data, seven):
-    for el in data:
-        if len(el & seven) == 2 and len(el) == 6:
-            data.remove(el) 
-            return el
+def cond_six(a, seven):
+    return len(a & seven) == 2 and len(a) == 6
 
-def find_seven(data):
-    for el in data:
-        if len(el) == 3:
-            data.remove(el) 
-            return el
+def cond_seven(a, _):
+    return len(a) == 3
 
-def find_eight(data):
-    for el in data:
-        if len(el) == 7:
-            data.remove(el) 
-            return el
+def cond_eight(a, _):
+    return len(a) == 7
 
-def find_nine(data, four):
+def cond_nine(a, four):
+    return len(a & four) == 4 and len(a) == 6
+
+def find_general(data, func, par):
     for el in data:
-        if len(el & four) == 4 and len(el) == 6:
-            data.remove(el) 
+        if func(el, par):
+            data.remove(el)
             return el
 
 if __name__ == '__main__':
