fd6d192c764e8a29cb3b9cc3fbf9d59cf6072219
Ryerzs <ryerzs@gmail.com>
2021-12-16 15:18:08 +0100

    More compact

--- Day-16/Stars.py
+++ Day-16/Stars.py
@@ -7,98 +7,82 @@ def main():
                 open(path, 'r').read().splitlines()]
 
     inp = data[0]
-    unraveled, ver, _ = unravel_inp(inp)
-    if type(unraveled) == int:
-        num = unraveled
+    num, version_sum, _ = unravel_inp(inp)
+    if not type(num) == int:
+        return False
 
     dt = time.time() - start_time
     print(num)
-    print(ver)
+    print(version_sum)
     print(dt)
 
-def literal(inp):
+def literal(inp_str):
     last = False
     packets = ''
     ind = 6
     while not last:
-        if inp[ind] == '0':
+        if inp_str[ind] == '0':
             last = True
-        packet = inp[ind+1:ind+5]
+        packet = inp_str[ind+1:ind+5]
         packets = packets + packet
         ind += 5
     num = int(packets, 2)
     return num, ind
 
-def unravel_inp(inp):
+def unravel_inp(inp_str):
     num = []
-    header = int(inp[0:3], 2)
-    ver = header
-    type_id = int(inp[3:6], 2)
+    version_sum = int(inp_str[0:3], 2)
+    type_id = int(inp_str[3:6], 2)
     if type_id == 4:
-        num, l = literal(inp)
-        return num, ver, l
-    length_id = int(inp[6:7])
+        num, tot_len = literal(inp_str)
+        return num, version_sum, tot_len
+
+    length_id = int(inp_str[6:7])
     if length_id == 0:
-        # The length bits is 15 bits
-        cur_len = 0
-        length = int(inp[7:22], 2)
-        rest = inp[22:]
-        while cur_len != length:
-            unraveled, v, l = unravel_inp(rest)
-            ver += v
-            if type(unraveled) == int:
-                num.append(unraveled)
-            cur_len += l
-            rest = rest[l:]
-        num = perform_operation(num, type_id)
-        tot_len = 22 + cur_len
+        cond = condition1
     else:
-        # The length bits is 11 bits
-        length = int(inp[7:18], 2)
-        rest = inp[18:]
-        cur_len = 0
-        for i in range(length):
-            unraveled, v, l = unravel_inp(rest)
-            cur_len += l
-            if type(unraveled) == int:
-                num.append(unraveled)
-            ver += v
-            rest = rest[l:]
-        num = perform_operation(num, type_id)
-        tot_len = 18 + cur_len
-    return num, ver, tot_len
+        cond = condition2
+
+    inp_str = inp_str[7:]
+    length_type = 15 - 4*length_id
+    rest = inp_str[length_type:]
+    length = int(inp_str[0:length_type], 2)
+    cur_len = 0
+    i = 0
+    while cond(cur_len, length, i):
+        n, v, l = unravel_inp(rest)
+        num.append(n)
+        version_sum += v
+        cur_len += l
+        rest = rest[l:]
+        i += 1
+    num = perform_operation(num, type_id)
+    tot_len = 7 + length_type + cur_len
+    return num, version_sum, tot_len
+
+def condition1(cur_len, length, i):
+    return cur_len != length
+
+def condition2(cur_len, length, i):
+    return i < length
 
 def perform_operation(num, op):
-    s = 0
+    s = 1
     if op == 0:
-        for n in num:
-            s += n
+        s = sum(num)
     elif op == 1:
-        s = 1
         for n in num:
             s *= n
     elif op == 2:
-        s = num[0]
-        for n in num:
-            if n < s:
-                s = n
+        s = min(num)
     elif op == 3:
-        s = num[0]
-        for n in num:
-            if n > s:
-                s = n
+        s = max(num)
     elif op == 5:
-        n1 = num[0]
-        n2 = num[1]
-        s = int(n1 > n2)
+        s = int(num[0] > num[1])
     elif op == 6:
-        n1 = num[0]
-        n2 = num[1]
-        s = int(n1 < n2)
+        s = int(num[0] < num[1])
     elif op == 7:
-        n1 = num[0]
-        n2 = num[1]
-        s = int(n1 == n2)
+        s = int(num[0] == num[1])
     return s
 
 if __name__ == '__main__':
